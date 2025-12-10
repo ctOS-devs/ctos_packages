cc43b16385ebca2258c0cab3d9fd1d48e9bb3eb3
justuser-31 <kirillmoscow2@gmail.com>
2025-12-10 08:53:55 +0000

    Первые попытки в сборку пакетов

---
+++ build_all.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Store the original directory
+ORIGINAL_DIR="$PWD"
+
+shopt -s expand_aliases
+alias rt="$PWD/rtracker"
+
+# Loop through each item in the current directory
+for dir in */; do
+    # Check if it's actually a directory (in case no directories exist)
+    if [ -d "$dir" ]; then
+#        echo "Entering directory: $dir"
+        cd "$dir" || continue
+        rt "Building $dir" \
+            %% abuild -r
+
+        cd "$ORIGINAL_DIR"
+    fi
+done
+
+echo "Completed processing all directories."
+echo "Output in: $HOME/packages/ctos_packages/x86_64/"
